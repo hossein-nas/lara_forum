6731111c1c81f0512ae628f4cd56c4790d363489
HOSSEIN NASIRI {UBUNTU} <hossein.nasiri.sovari@gmail.com>
2020-05-12 13:40:32 +0000

    Lesson 05

--- resources/views/threads/show.blade.php
+++ resources/views/threads/show.blade.php
@@ -24,5 +24,22 @@
                 @endforeach
             </div>
         </div>
+
+        @if( auth()->check() ) 
+            <div class="row">
+                <div class="col-md-8 col-md-offset-2">
+                    <form action="{{ $thread->path() . '/replies' }}" method="POST" >
+                        {{ csrf_field() }}
+                        <div class="form-group">
+                            <textarea name="body" id="body" cols="30" rows="5" class="form-control" placeholder="Have something to say?"></textarea>
+                        </div>
+                        <button class="btn btn-default" type="submit">POST</button>
+                        
+                    </form>
+                </div>
+            </div>
+        @else
+            <p class="text-center">Please <a href="{{ route('login') }}">sign in</a> to participate in this dicussion.</p>
+        @endif
     </div>
 @endsection

0e43200ec0c0dacb56b2618ac6b686fbe37da438
HOSSEIN NASIRI {UBUNTU} <hossein.nasiri.sovari@gmail.com>
2020-05-16 12:42:52 +0000

    Lesson 19

--- app/Reply.php
+++ app/Reply.php
@@ -29,5 +29,11 @@ class Reply extends Model
     	]);
     }
     
+
+    public function isFavorited()
+    {
+    	return $this->favorites()->where('user_id', auth()->id() )->exists();	
+    }
+    
     
 }
--- resources/views/threads/reply.blade.php
+++ resources/views/threads/reply.blade.php
@@ -1,8 +1,29 @@
 <div class="panel panel-default">
     <div class="panel-heading">
-        <a href="#">
-            {{ $reply->owner->name }}
-        </a> said {{ $reply->created_at->diffForHumans() }}...
+
+        <div class="level">
+        	<span class="flex">
+        		<a href="#" >
+        		    {{ $reply->owner->name }}
+        		</a> said {{ $reply->created_at->diffForHumans() }}...
+        	</span>
+
+        	<div>
+        		@if(! $reply->isFavorited())
+	        		<form method="POST" action="/replies/{{ $reply->id }}/favorites">
+	        			{{ csrf_field() }}
+	        			<button type="submit" class="btn btn-default">
+	        				{{ $reply->favorites()->count() }} {{ str_plural('Favorite', $reply->favorites()->count() )}}
+	        			</button>
+	        		</form>
+
+	        	@else
+        			<button type="submit" class="btn btn-default" disabled="disabled">
+        				 Favorited ({{ $reply->favorites()->count() }})
+        			</button>
+	        	@endif
+        	</div>
+        </div>
     </div>
 
     <div class="panel-body">

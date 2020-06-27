c02b0681bef6786332d240a76f3d1beca1b869a9
HOSSEIN NASIRI {UBUNTU} <hossein.nasiri.sovari@gmail.com>
2020-06-27 12:24:38 +0430

    Lesson 47

--- app/Thread.php
+++ app/Thread.php
@@ -93,6 +93,8 @@ class Thread extends Model
         $this->subscriptions()->create([
             'user_id' => $userId ?: auth()->id()
         ]);
+
+        return $this;
     }
 
     /*
--- tests/Unit/ThreadTest.php
+++ tests/Unit/ThreadTest.php
@@ -2,10 +2,12 @@
 
 namespace Tests\Unit;
 
-use App\Thread;
 use App\User;
-use Illuminate\Foundation\Testing\DatabaseMigrations;
+use App\Thread;
 use Tests\TestCase;
+use App\Notifications\ThreadWasUpdated;
+use Illuminate\Support\Facades\Notification;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
 
 class ThreadTest extends TestCase
 {
@@ -44,6 +46,23 @@ class ThreadTest extends TestCase
 
         $this->assertCount(1, $this->thread->replies);
     }
+
+    /** @test */
+    public function a_thread_notifies_all_registered_subscribers_when_a_reply_is_added()
+    {
+        Notification::fake();
+
+        $this->signIn()
+            ->thread
+            ->subscribe()
+            ->addReply([
+                'body'      => 'foobar',
+                'user_id'   => 999
+            ]);
+
+        Notification::assertSentTo(auth()->user(), ThreadWasUpdated::class);
+    }
+    
 
     /** @test */
     public function a_thread_belongs_to_channel()
